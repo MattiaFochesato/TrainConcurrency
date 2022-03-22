458095558f3b7b59e7d91def5d96fc31cc5d1add
Mattia Fochesato <36085165+MattiaFochesato@users.noreply.github.com>
2022-03-22 09:53:21 +0100

    Initial Commit

---
+++ TrainConcurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrainConcurrency
+//
+//  Created by Mattia Fochesato on 22/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrainConcurrency/TrainConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainConcurrencyApp.swift
+//  TrainConcurrency
+//
+//  Created by Mattia Fochesato on 22/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TrainConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
